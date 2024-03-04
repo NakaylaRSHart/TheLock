2b7577bc326c34173af831ebfbbe1f40cef27254
NakaylaRSHart <nakaylahart@DESKTOP-SJTLOE9>
2024-03-04 12:18:40 -0700

    Created an HTML and Main Java file.

---
+++ main.js
@@ -0,0 +1,21 @@
+const numCows = 50;
+
+const helloWorld='Hello World'
+
+console.log(helloWorld)
+console.log("Hello World")
+
+console.log("Hello" + "Disneyland")
+
+const numUmbrella = 10 * 3;
+
+const num = 5 + 1;
+console.log(num)
+
+console.log("Hello" + " " + "World")
+
+const str1 = "Hello,";
+const str2 = "I am";
+const aName = "Josh";
+alert(str1 + " " + str2 + " " + aName);
+
