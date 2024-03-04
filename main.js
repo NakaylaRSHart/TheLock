166690d0b2e4bd0e1a8593988d88fb8560c24679
NakaylaRSHart <nakaylahart@DESKTOP-SJTLOE9>
2024-03-04 14:02:02 -0700

    Made a css sheet as well as tried to finish/fix the lock combination check.

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
-const numCows = 50;
+/*
+SECURE THE VAULT
 
-const helloWorld='Hello World'
+1. Create string so user knows when vault is unlocked/locked
+2. Use 3 numbers(variables) for lock code combination.(10-40-39).
+3. Created Diablog box displaying the vault code.
+4. Create a pop up at the start that says "Find the code!"
 
-console.log(helloWorld)
-console.log("Hello World")
+***Be SURE TO COMMENT STEPS IN CODE***
+*/
 
-console.log("Hello" + "Disneyland")
+//Creating alert to tell user to find the code-------
+alert("Find the Code!");
 
-const numUmbrella = 10 * 3;
+//Setting combination--------------------------------
+const correctCombo = [10 , 40, 39]
+const number1 = 10;
+const number2 = 40;
+const number3 = 39;
 
-const num = 5 + 1;
-console.log(num)
+//Funciton to get combination------------------------
+function checkCombo(){
+  // Get the entered combination
+const number1 = document.number1('number1');
+const number2 = document.number2('number2');
+const number3 = document.number3('number3');
+}
+ 
+//Checking if combination is correct; if it is show unlocked, else show still locked.
 
-console.log("Hello" + " " + "World")
-
-const str1 = "Hello,";
-const str2 = "I am";
-const aName = "Josh";
-alert(str1 + " " + str2 + " " + aName);
-
+if(userAnswer.number1 === correctCombo[0] && number2 === correctCombo[1] && number3 === correctCombo[2]){
+  alert =("result") ('Unlocked!!');
+}
+else{
+  alert =("result")('Incorrect!! Try Again!');
+}
